509d20b5489bf9dac04fee41657918c25fe137a4
Anton Siadun <antochaleo@gmail.com>
2022-11-24 00:47:53 +0300

    feat: add matrix interface
    This interface presents a two-dimentional array, contained base methods of getting and setting values.

---
+++ SimpleAlgorithms/IMatrix.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleAlgorithms
+{
+    public interface IMatrix<T>
+    {
+        public int Width { get; }
+        public int Height { get; }
+
+        public T this[int row, int column] { get; set; }
+
+        public T[,] GetArray();
+    }
+}
