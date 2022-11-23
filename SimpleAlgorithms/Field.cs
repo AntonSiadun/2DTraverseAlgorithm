9563fb27b1b1ddf2e4f66bdef41a73afd24f9d54
Anton Siadun <antochaleo@gmail.com>
2022-11-24 00:48:42 +0300

    feat: add class, implements matrix
    Base functionality of getting data from matrix, but safety and from interface. GetArray method returns copy, not reference.

---
+++ SimpleAlgorithms/Field.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SimpleAlgorithms
+{
+    public class Field<T> : IMatrix<T>
+    {
+        private T[,] _array;
+
+        public Field(int width, int height)
+        {
+            if (width <= 0 || height <= 0)
+                throw new ArgumentException("Widht and height must be positive values.");
+
+            _array = new T[width, height];
+        }
+
+        public T this[int row, int column]
+        {
+            get => _array[row, column];
+            set => _array[row, column] = value;
+        }
+
+        public int Width => _array.GetLength(1);
+        public int Height => _array.GetLength(0);
+
+        public T[,] GetArray()
+        {
+            T[,] result = new T[Height, Width];
+
+            for(int i = 0; i < Height; i++)
+            {
+                for(int j = 0; j < Width; j++)
+                {
+                    result[i, j] = _array[i, j];
+                }
+            }
+            return result;
+        }
+    }
+}
