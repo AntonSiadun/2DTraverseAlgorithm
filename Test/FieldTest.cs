5d32dcc6e2a260df0cca1e85ad47356e4f74f2b0
Anton Siadun <antochaleo@gmail.com>
2022-11-27 15:11:12 +0300

    test: adapt field test for Vector2Int
    -//-

--- Test/FieldTest.cs
+++ Test/FieldTest.cs
@@ -44,6 +44,16 @@ namespace Test
             Assert.Throws<ArgumentException>(() => new Field<int>(height, 1));
         }
 
+        [Test]
+        public void Initialization_WithArray_ArrayIsTheSame()
+        {
+            int[,] array = new int[1, 3] { { 1, 2, 3 } };
+
+            var field = new Field<int>(array);
+
+            Assert.AreEqual(array[0, 0], field.GetArray()[0, 0]);
+        }
+
         //Item reading
         [Test]
         public void Reading_GetWidth_ReturnCorrectValue()
@@ -73,7 +83,8 @@ namespace Test
             var field = new Field<int>(height, width);
             int result;
 
-            Assert.Throws<IndexOutOfRangeException>(() => result = field[rowInPosition, colInPosition]);
+            Assert.Throws<IndexOutOfRangeException>(() =>
+                        result = field[new Vector2Int(rowInPosition, colInPosition)]);
         }
 
         //Item saving
@@ -83,10 +94,24 @@ namespace Test
             var width = 10;
             var height = 10;
             var rowInPosition = 11;
+            var colInPosition = 5;
+            var field = new Field<int>(height, width);
+
+            Assert.Throws<IndexOutOfRangeException>( () =>
+                        field[new Vector2Int(rowInPosition,colInPosition)] = 0);
+        }
+
+        [Test]
+        public void Saving_NegativePosition_ThrowsException()
+        {
+            var width = 10;
+            var height = 10;
+            var rowInPosition = 5;
             var colInPosition = -1;
             var field = new Field<int>(height, width);
 
-            Assert.Throws<IndexOutOfRangeException>( () => field[rowInPosition,colInPosition] = 0);
+            Assert.Throws<IndexOutOfRangeException>(() =>
+                        field[new Vector2Int(rowInPosition, colInPosition)] = 0);
         }
 
         [Test]
@@ -99,7 +124,7 @@ namespace Test
             var field = new Field<int>(height, width);
             var value = 3;
 
-            field[rowInPosition, colInPosition] = value;
+            field[new Vector2Int(rowInPosition, colInPosition)] = value;
             var array = field.GetArray();
 
             Assert.AreEqual(value, array[rowInPosition, colInPosition]);
@@ -129,7 +154,7 @@ namespace Test
             var colPosition = 0;
             var value = 6;
 
-            field[rowPosition, colPosition] = value;
+            field[new Vector2Int(rowPosition, colPosition)] = value;
             var array = field.GetArray();
 
             Assert.AreEqual(value, array[rowPosition, colPosition]);
