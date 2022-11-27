bbf9beb7608777da564b87c2a7c960d437ad6a84
Anton Siadun <antochaleo@gmail.com>
2022-11-27 15:12:51 +0300

    test: add test examples in separate class
    -//-

---
+++ Test/TestExamples.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using SimpleAlgorithms;
+
+namespace Test
+{
+    public class TestExamples
+    {
+        public readonly Dictionary<int[,], List<Vector2Int>> Examples;
+
+        public TestExamples()
+        {
+            Examples = new Dictionary<int[,], List<Vector2Int>>();
+
+            Examples.Add(new int[1, 3] { { 1, 1, 1 } },
+                new List<Vector2Int> { new Vector2Int(0, 0),
+                new Vector2Int(0, 1), new Vector2Int(0, 2) });
+
+            Examples.Add(new int[2, 3] { { 1, 1, 1 }, { 0, 1, 2 } },
+                new List<Vector2Int> { new Vector2Int(0, 0),
+                new Vector2Int(0, 1), new Vector2Int(0, 2),
+                new Vector2Int(1, 1)});
+
+            Examples.Add(new int[3, 3] { { 3, 1, 0 }, { 1, 1, 1 }, { 0, 1, 2 } },
+                new List<Vector2Int> { new Vector2Int(0, 1),
+                new Vector2Int(1, 0), new Vector2Int(1, 1),
+                new Vector2Int(1, 2), new Vector2Int(2, 1)});
+
+            Examples.Add(new int[3, 3] { { 1, 0, 2 }, { 0, 0, 3 }, { 0, 1, 1 } },
+                new List<Vector2Int> { new Vector2Int(0, 1),
+                new Vector2Int(1, 1), new Vector2Int(1, 0),
+                new Vector2Int(2, 0)});
+
+            Examples.Add(new int[3, 3] { { 1, 0, 0 }, { 1, 0, 0 }, { 0, 1, 1 } },
+                new List<Vector2Int> { new Vector2Int(0, 1),
+                new Vector2Int(1, 1), new Vector2Int(1, 2),
+                new Vector2Int(0, 2)});
+
+            Examples.Add(new int[4, 3] { { 0, 1, 0 }, { 1, 1, 1 }, { 2, 2, 1 }, { 0, 1, 1 } },
+                new List<Vector2Int> { new Vector2Int(0, 1),
+                new Vector2Int(1, 0), new Vector2Int(1, 1),
+                new Vector2Int(1, 2), new Vector2Int(2, 2),
+                new Vector2Int(3, 2), new Vector2Int(3, 1)});
+        }
+    }
+}
