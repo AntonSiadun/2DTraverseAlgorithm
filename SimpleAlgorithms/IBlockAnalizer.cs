c0ef351b2e7b7a13683f2a508c5068978fee3e9c
Anton Siadun <antochaleo@gmail.com>
2022-11-27 15:55:12 +0300

    refactor: change return type GetAllBlocks()
    -//-

--- SimpleAlgorithms/IBlockAnalizer.cs
+++ SimpleAlgorithms/IBlockAnalizer.cs
@@ -5,7 +5,7 @@ namespace SimpleAlgorithms
     public interface IBlockAnalizer
     {
         //Finding block of objects in field
-        public HashSet<Vector2Int>[] GetAllBlocks();
+        public List<List<Vector2Int>> GetAllBlocks();
         public HashSet<Vector2Int> GetIncludedBlock(Vector2Int position);
         //Check, that object is included in some block
         public bool IsIncluded(Vector2Int position);
