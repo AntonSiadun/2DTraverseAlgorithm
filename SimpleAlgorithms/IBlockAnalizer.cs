6f1f3e28624ed244379d862d03234e7bce5d166b
Anton Siadun <antochaleo@gmail.com>
2022-11-27 15:11:44 +0300

    feat: add interface for BlockAnalizer
    -//-

---
+++ SimpleAlgorithms/IBlockAnalizer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleAlgorithms
+{
+    public interface IBlockAnalizer
+    {
+        //Finding block of objects in field
+        public HashSet<Vector2Int>[] GetAllBlocks();
+        public HashSet<Vector2Int> GetIncludedBlock(Vector2Int position);
+        //Check, that object is included in some block
+        public bool IsIncluded(Vector2Int position);
+    }
+}
