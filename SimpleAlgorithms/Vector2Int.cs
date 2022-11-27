9258af3f6c08add6d320a6a02285e8b5fbd58b12
Anton Siadun <antochaleo@gmail.com>
2022-11-27 15:08:20 +0300

    feat: add structure to navigate in matrix
    Vector2Int contains two fields, a row and column, that represents a position or gap in matrix. Has grounds operations: add and substract, and ToString() realization.

---
+++ SimpleAlgorithms/Vector2Int.cs
@@ -0,0 +1,31 @@
+﻿namespace SimpleAlgorithms
+{
+    public struct Vector2Int
+    {
+        public readonly int Column;
+        public readonly int Row;
+
+        public Vector2Int(int row, int column)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public static Vector2Int operator +(Vector2Int vectorA, Vector2Int vectorB)
+        {
+            return new Vector2Int(vectorA.Row + vectorB.Row,
+                                  vectorA.Column + vectorB.Column);
+        }
+
+        public static Vector2Int operator -(Vector2Int vectorA, Vector2Int vectorB)
+        {
+            return new Vector2Int(vectorA.Row - vectorB.Row,
+                                  vectorA.Column - vectorB.Column);
+        }
+
+        public override string ToString()
+        {
+            return "row: "+Row+", column: "+Column;
+        }
+    }
+}
